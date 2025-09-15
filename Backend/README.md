e77aaa14038441ba0d87d5d4aaa8db24341d1da6
AbhinavReddy10 <abhinav.kondakindi@gmail.com>
2025-09-15 14:22:54 +0530

    Delete Backend directory

--- Backend/README.md
+++
@@ -1,3 +0,0 @@
-# User Management REST API
-
-Run `npm install` and `npm run dev` to start the project.
--- Backend/user-management-api/models/index.js
+++
@@ -1,5 +0,0 @@
-const { Sequelize } = require('sequelize');
-const config = require('../config/config');
-const sequelize = new Sequelize({ dialect: config.database.dialect, storage: config.database.storage, logging: config.database.logging });
-const User = require('./user')(sequelize);
-module.exports = { sequelize, User };
