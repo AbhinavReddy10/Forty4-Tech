eeeecee7a2032027826bdda3ef4bdde774d5caf7
AbhinavReddy10 <abhinav.kondakindi@gmail.com>
2025-09-15 14:21:36 +0530

    Delete Backend/user-management-api/utils directory

--- Backend/user-management-api/utils/validators.js
+++
@@ -1,3 +0,0 @@
-const { body } = require('express-validator');
-exports.validateUserCreation = [ body('name').notEmpty(), body('email').isEmail() ];
-exports.validateUserUpdate = [ body('email').optional().isEmail() ];
