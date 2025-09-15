4815f25725b138f7cf3639150b7d5b766821b65e
AbhinavReddy10 <abhinav.kondakindi@gmail.com>
2025-09-15 12:35:57 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios'
-
-export const fetchUsers = () =>
-  axios.get('https://jsonplaceholder.typicode.com/users').then(res => res.data)
--- src/components/SearchBar.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function SearchBar({ value, onChange }) {
-  return <input className="form-control" placeholder="Search by name" value={value} onChange={e => onChange(e.target.value)} />
-}
